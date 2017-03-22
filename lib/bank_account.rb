d7003d1ac9383fb26e1d083d52ec3ee87f906333
ethanryan <ethanryan@gmail.com>
2017-03-22 15:55:26 -0400

    done

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,41 @@
 class BankAccount
 
-end
+  attr_reader :name #reader instead of accessor cuz don't want users changing their names
+                    #and cuz for now, just want to get it passing with minimal number of methods
+                    #accessor would create two methods, reader and writer
+
+  attr_accessor :balance, :status #accessor cuz balance will change
+
+  def initialize(name)
+    @name = name
+    @balance = 1000 #@balance == self.balance <<<---this is the reader method (correct??)
+    @status = 'open'
+  end #end method
+
+  def deposit(amount)
+    #@balance += amount #this works, but better to use self.balance below, so the method balance is being called on instances instead of using the instance variable balance
+    ## is self.balance THE SAME AS @balance???
+    self.balance += amount #need self to call method on each instance of bank account
+    #self.balance=(balance + amount) ###above line is same as this
+    #self.balance= above is the reader method (correct??)
+    #(balance + amount) is the writer method  (correct??)
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid? #question mark means a boolean, want to return true or false
+    status == "open" && balance > 0 #don't need below, this is all you need here, cuz it returns true if true, false if not
+    # if status == "open" && balance > 0
+    #   true
+    # else
+    #   false
+    # end #end if else
+  end
+
+  def close_account
+    self.status = "closed"
+  end
+
+end #end class
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,39 @@
 class Transfer
-  # your code here
-end
+
+  attr_reader :receiver, :sender, :amount
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @receiver = receiver
+    @sender = sender
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid? #isn't it confusing to use same method name in both classes???
+    #receiver.valid? && sender.valid?
+    self.receiver.valid? && self.sender.valid?
+    #self is implicit above, can do with or without, adding to make explicit
+    #.receiver and .sender are being called on BankAccount class instances
+  end
+
+  def execute_transaction
+    if self.valid? == true && self.sender.balance > amount && self.status == "pending"
+      self.sender.balance -= self.amount   #decreases by amount
+      self.receiver.balance += self.amount #increases by amount
+      self.status = "complete" #transfer complete
+    else
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end #end if statement
+  end
+
+  def reverse_transfer
+    if self.status == "complete" #if transfer was completed...
+      self.sender.balance += self.amount   #increase by amount
+      self.receiver.balance -= self.amount #decrease by amount
+      self.status = "reversed" #reversed transfer
+    end #end if statement
+  end
+
+end #end class
